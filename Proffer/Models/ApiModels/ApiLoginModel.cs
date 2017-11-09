dfae87a515f90a64d787ba7954d06815f64248de
TheoreticallyABanana <Jeffreylee6006@hotmail.com>
2017-11-09 21:24:16 +0800

    Created api with authenication token, product and login models

---
+++ Proffer/Models/ApiModels/ApiLoginModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proffer.Models.ApiModels
+{
+    public class ApiLoginModel
+    {
+        [Key]
+        [ForeignKey("User_id")]
+        public int User_id { get; set; }
+
+        [Required]
+        public string User_name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Proffer/Models/ApiModels/ProductModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proffer.Models.ApiModels
+{
+    public class ProductModel
+    {
+        [Required]
+        [Key]
+        public int Product_id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public float Price { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Product_tag { get; set; }
+
+        [ForeignKey("User_id")]
+        public ApiLoginModel User {get; set;}
+    }
+
+}
